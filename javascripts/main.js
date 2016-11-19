6b6ca1c53d3fd04dda44dff7b87c4cd903def4aa
Eliseo Torres <eliseo.torres95@gmail.com>
2016-11-19 02:11:19 -0500

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,10 +1,9 @@
-if ('serviceWorker' in navigator) {                                   // Primero validamos que el navegador soporte SW
-
-  navigator.serviceWorker.register('/sw.js').then(registration => {   // A continuación invocamos el método register indicando la ruta del archivo del SW
-                                                                      // Debe estar al mismo nivel de index.html,
-                                                                      //   en la raíz de nuestra app para que tenga acceso a todos los archivos
-    console.log('Service worker registrado :D');
-  }).catch(err => {
-    console.log(':( Algo pasó: ', err);
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    // Registration was successful
+    console.log('ServiceWorker registrado: ', registration.scope);
+  }).catch(function(err) {
+    // registration failed :(
+    console.log('ServiceWorker no registrado fuck: ', err);
   });
 }
