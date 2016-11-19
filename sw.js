7c06aa174788dffd053430b56145fd8cc0d9be4d
Eliseo Torres <eliseo.torres95@gmail.com>
2016-11-19 15:19:01 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -9,16 +9,13 @@ const archivos_cache = [       // Array de las urls de los archivos que quiero g
   
   
 ];
-
-self.addEventListener('install', event => {         // self es lo mismo que 'this'
-  
-  event.waitUntil(                                  // Recibe una promesa y actúa dependiendo de su resolución
-    caches.open(FILE_CACHE)                           // ... Es claro qué hace
-      .then(cache => {
-        return cache.addAll(archivos_cache); // Una vez se abre la caché se agregan a ella todos los archivos especificados
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(FILE_CACHE)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(archivos_cache);
       })
-  ).then(() => {
-    return self.skipWaiting();                      // El SW pasa de estado 'instalando' a 'activado'
-  });
-
+  );
 });
