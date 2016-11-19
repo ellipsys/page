a88492c4dc0322e9c11fc660da2c27039bd33270
Eliseo Torres <eliseo.torres95@gmail.com>
2016-11-19 15:17:22 -0500

    Update sw.js

--- sw.js
+++ sw.js
@@ -3,9 +3,9 @@
 const FILE_CACHE = 'v.1'; // El nombre de la caché que vamos a usar. 
 const archivos_cache = [       // Array de las urls de los archivos que quiero guardar en caché
   '/',                                
-  '/javascripts/main.js',
-  '/images/x-icon.png',
-  '/stylesheets/stylesheet.css',
+  'javascripts/main.js',
+  'images/x-icon.png',
+  'stylesheets/stylesheet.css',
   
   
 ];
