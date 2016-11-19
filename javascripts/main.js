8d06f57935647564e27a43f20ca85dba4cf0c311
Eliseo Torres <eliseo.torres95@gmail.com>
2016-11-19 01:23:29 -0500

    Update main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,2 +1,9 @@
-console.log('This would be the main JS file.');
-console.alert("Bienvenidos");
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    // Registration was successful
+    console.log('ServiceWorker registrado ', registration.scope);
+  }).catch(function(err) {
+    // registration failed :(
+    console.log('ServiceWorker no registrado: ', err);
+  });
+}
