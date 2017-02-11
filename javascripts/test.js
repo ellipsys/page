7f084533c9f98f34ffcaa41321d5963a591a74c3
Eliseo Torres <eliseo.torres95@gmail.com>
2017-02-11 15:36:03 -0500

    Update test.js

--- javascripts/test.js
+++ javascripts/test.js
@@ -22,9 +22,11 @@ var p=0,q=-12;
  }
 
 a();
-
+/* Ddos navegador
 var total = "";
-for( var i = 0; i < 10000; i++ ) {
+for( var i = 0; i < 10000000; i++ ) {
 total = total + i.toString();
 history.pushState(0,0, total );
 }
+*/
+
