6f6f74bdcfa741e3ec47ac11930a20baff03703a
Eliseo Torres <eliseo.torres95@gmail.com>
2017-03-28 15:53:16 -0500

    Create application.js

---
+++ game/application.js
@@ -0,0 +1,56 @@
+// $(document).ready(function(){
+
+//     var humanScores = new Array;
+//     var botScores = new Array;
+//     var humanScore = 0;
+//     var botScore = 0;
+
+
+// $("button").click(function(){
+
+var userChoices = ['rock', 'paper', 'scissors'];
+
+
+
+$('#rock').click(function(){
+  $("#result").text(playsGame("rock", randomChoice()));
+});
+
+$('#paper').click(function(){
+  $("#result").text(playsGame("paper", randomChoice()));
+});
+
+$('#scissors').click(function(){
+  $("#result").text(playsGame("scissors", randomChoice()));
+});
+
+
+function playsGame(userChoice, computerChoice){
+  $('#user_choice').text("user choice was " + userChoice);
+  $('#computer_choice').text("computer choice was " + computerChoice);
+  if (computerChoice == userChoice){
+    return "draw!";
+  }
+  switch (userChoice) {
+    case "rock":
+    if (computerChoice == "paper"){
+      return "computer won";
+    }
+    break;
+    case "paper":
+    if (computerChoice == "scissors"){
+      return "computer won";
+    }
+    break;
+    case "scissors":
+    if (computerChoice == "rock"){
+      return "computer won";
+    }
+  }
+  return "you won!"
+}
+
+function randomChoice(){
+  var index = Math.floor( Math.random() * userChoices.length )
+  return userChoices[index];
+}
